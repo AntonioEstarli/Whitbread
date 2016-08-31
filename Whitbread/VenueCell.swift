a7934848ba7c1f42c1010a8f385b01b8292ed922
Antonio.Estrali <antonio.estrali@lebara.com>
2016-08-31 23:06:41 +0100

    - Basic View with search and tableView - VenueCell added

---
+++ Whitbread/VenueCell.swift
@@ -0,0 +1,15 @@
+//
+//  VenueCell.swift
+//  Whitbread
+//
+//  Created by Antonio.Estrali on 31/08/2016.
+//  Copyright © 2016 Antonio.Estrali. All rights reserved.
+//
+
+import UIKit
+
+class VenueCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+}
