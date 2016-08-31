f45c501c48fcaa77db670034623b11c7ee2c5fba
Antonio.Estrali <antonio.estrali@lebara.com>
2016-08-31 22:35:30 +0100

    - Added Venue simple struct and tests

---
+++ Whitbread/Venue.swift
@@ -0,0 +1,13 @@
+//
+//  Venue.swift
+//  Whitbread
+//
+//  Created by Antonio.Estrali on 31/08/2016.
+//  Copyright © 2016 Antonio.Estrali. All rights reserved.
+//
+
+struct Venue {
+    let name: String
+    let formattedAddress: String
+}
+
---
+++ WhitbreadTests/VenueTests.swift
@@ -0,0 +1,27 @@
+//
+//  VenueTests.swift
+//  Whitbread
+//
+//  Created by Antonio.Estrali on 31/08/2016.
+//  Copyright © 2016 Antonio.Estrali. All rights reserved.
+//
+
+import XCTest
+@testable import Whitbread
+
+class VenueTests: XCTestCase {
+
+    func testInit_ShouldTakeNameAndAddress() {
+        let venue = Venue(name: "Test name", formattedAddress: "Test Address")
+        XCTAssertNotNil(venue, "venue should not be nil")
+    }
+    
+    func testInit_ShouldSetNameAndAddress() {
+        let venue = Venue(name: "Test name", formattedAddress: "Test Address")
+        XCTAssertEqual(venue.name , "Test name",
+                       "Initializer should set the venue name")
+        XCTAssertEqual(venue.formattedAddress , "Test Address",
+                       "Initializer should set the venue address")
+    }
+
+}
