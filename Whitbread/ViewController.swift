cf30a2b5b110019e36c3a8d18a749338c9ba7ef4
Antonio.Estrali <antonio.estrali@lebara.com>
2016-08-31 15:50:23 +0100

    Initial Commit

---
+++ Whitbread/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whitbread
+//
+//  Created by Antonio.Estrali on 31/08/2016.
+//  Copyright © 2016 Antonio.Estrali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
